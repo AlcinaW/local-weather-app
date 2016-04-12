dd18c05d2b75ca02dce91a28d1a3d23b14838cae
AlcinaW <white_kaitou@hotmail.com>
2016-04-11 23:15:34 -0400

    API link move

--- js/app.js
+++ js/app.js
@@ -6,17 +6,19 @@
 //API key: 4ecd95152125036caf092f9322ecc291 (don't expose API keys normally)
 //JSON format 
 
-//ip API: http://ip-api.com/docs/api:json
+//If using IP API, won't need to request browser permission for geolocation?
+//callback with lat/lon passed?
+//on load, get geolocation of user 
+// window.onload = function getGeoLatLong() {
 
 //icon image associated with weather -> can use your own
 
-//IP API JSONP info
-// <script>
-// function JSONPHandler(data) {
-//   console.log(data);
-// }
-// </script>
-// <script src="http://ip-api.com/json/?callback=JSONPHandler"></script>
+var apiLink = "http://api.openweathermap.org/data/2.5/weather?q=";
+var apiKey = "&APPID=4ecd95152125036caf092f9322ecc291";
+
+//var weatherJSON = apiLink + location + apiKey;
+//console.log("weatherJSON");
+
 
 var xmlhttp = new XMLHttpRequest();
 var url = "http://ip-api.com/json";
@@ -29,47 +31,20 @@ xmlhttp.onreadystatechange = function() {
         console.log(response);
         document.getElementById("ipLat").innerHTML = response.lat;
         document.getElementById("ipLon").innerHTML = response.lon;
-        document.getElementById("output").innerHTML = response.city + ", " + response.countryCode;
+        document.getElementById("output").innerHTML = response.city + ", " + response.region + ", " + response.countryCode;
     }
 };
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
 
-//If using IP API, won't need to request browser permission for geolocation?
-//callback with lat/lon passed?
-//on load, get geolocation of user 
-// window.onload = function getGeoLatLong() {
-// 	var startPos;
-// 	var geoOptions = { 
-		//if can't get data, timeout 
-		// timeout: 10 * 1000,
-		//can use old location set instead of checking every time
-	// 	maximumAge: 5 * 60 * 1000
-	// };
-	//get latitude and longitude, output to page 
-	// var geoSuccess = function(position){
-	// 	startPos = position;
-	// 	console.log(startPos);
-	// 	document.getElementById("startLat").innerHTML = startPos.coords.latitude;
-	// 	console.log(startPos.coords.latitude);
-	// 	document.getElementById("startLon").innerHTML = startPos.coords.longitude;
-	// 	console.log(startPos.coords.longitude);
-	// };
-	//in case of error, return error
-// 	var geoError =  function(error){
-// 		console.log("Error has occured. Error code:" + error.code + " - " +  error.message);
-// 	};
-// 	navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
-// };
-
-
-//check for geolocation enabled/supported
-// if (navigator.geolocation){
-// 	console.log("Geolocation is supported~");
-// } else {
-// 	console.log("Awww, geolocation is not supported for this browser yet.");
-// }
+// Function to work with Location API to get Longitude, Latitude, City and State to bed used with the weather API
+var getLocation = function (response) {
+	var city = response.city;
+	var lat = response.lat;
+	var lon = response.lon;
+	url = apiLink + "lat=" + lat + "&lon=" + lon + "&units=" + apiKey;
+}
 
 //ip API: http://ip-api.com/docs/api:json
 //function getIpApi(navigator.geolocation){
@@ -77,11 +52,8 @@ xmlhttp.send();
 	//getWeather(callback);
 //}
 
-//var apiLink = "http://api.openweathermap.org/data/2.5/weather?q=";
-//var apiKey = "&APPID=4ecd95152125036caf092f9322ecc291";
 
-//var weatherJSON = apiLink + location + apiKey;
-//console.log("weatherJSON");
+
 
 
 //function updateWeather(weather){
